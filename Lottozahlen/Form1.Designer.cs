2f9653a5dbdc6870e8bc96497dd88dd4d4c91571
notbinmelone <1058088@bkrnet.de>
2022-12-01 11:33:44 +0100

    Design, functionality

--- Lottozahlen/Form1.Designer.cs
+++ Lottozahlen/Form1.Designer.cs
@@ -28,13 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Font = new System.Drawing.Font("ROG Fonts", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(186, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(458, 74);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GENERATE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 86);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "99";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(159, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 86);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "99";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gold;
+            this.label3.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(399, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 86);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "99";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(279, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 86);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "99";
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Gold;
+            this.label5.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(519, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 86);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "99";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Gold;
+            this.label6.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(639, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 86);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "99";
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Gold;
+            this.label7.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(639, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 86);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "99";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gold;
+            this.pictureBox1.Image = global::Lottozahlen.Properties.Resources.lottobg;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-13, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(829, 447);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Gold;
+            this.label8.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(39, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(714, 58);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "IHRE LOTTOZAHLEN:";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Gold;
+            this.label9.Font = new System.Drawing.Font("ROG Fonts", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(186, 178);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(435, 58);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "SUPERZAHL:";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 394);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Lotto";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- Lottozahlen/Form1.cs
+++ Lottozahlen/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,43 @@ namespace Lottozahlen
         public Form1()
         {
             InitializeComponent();
+            Main();
+        }
+        Random random = new Random();
+        int[] numbers = new int[7];
+        void Main()
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                    numbers[i] = random.Next(1, 49);
+            }
+            label1.Text = Convert.ToString(numbers[0]);
+            label2.Text = Convert.ToString(numbers[1]);
+            label3.Text = Convert.ToString(numbers[2]);
+            label4.Text = Convert.ToString(numbers[3]);
+            label5.Text = Convert.ToString(numbers[4]);
+            label6.Text = Convert.ToString(numbers[5]);
+            label7.Text = Convert.ToString(numbers[6]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 49);
+            }
+            label1.Text = Convert.ToString(numbers[0]);
+            label2.Text = Convert.ToString(numbers[1]);
+            label3.Text = Convert.ToString(numbers[2]);
+            label4.Text = Convert.ToString(numbers[3]);
+            label5.Text = Convert.ToString(numbers[4]);
+            label6.Text = Convert.ToString(numbers[5]);
+            label7.Text = Convert.ToString(numbers[6]);
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
